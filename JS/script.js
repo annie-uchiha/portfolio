a8028ffa7a14f471001ae959a218a93d658ebed3
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-03-26 00:00:48 +0200

    trying to fix it

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,6 @@ const btnNav - document.querySelector(".btn-nav");
 
 const list = document.querySelector("ul");
 
-btn-nav.addEventListener("click", () => {
-    list.classList.toggle("ul");
+btnNav.addEventListener("click", () => {
+    list.classList.toggle("active");
 })
