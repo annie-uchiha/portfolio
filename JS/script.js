bb8d7e6f6bf8946ebb28f294b802c4bb9116eab8
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-03-31 00:16:41 +0200

    js homework

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,15 @@ const list = document.querySelector("ul");
 btn.addEventListener("click", () => {
   list.classList.toggle("active");
 });
+
+function greetUser() {
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("greeting").textContent = `Hello,${name}`;
+}
+
+function calculateAge() {
+  const currentYear = new Date().getFullYear();
+  const birthYear = parseInt(document.getElementById("birthYear").value);
+  const age = currentYear - birthYear;
+  document.getElementById("age").textContent = "Your age is: " + age;
+}
