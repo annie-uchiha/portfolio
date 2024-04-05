7b2222e71bb51e82321929f15655016ca29cbcb7
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-04-05 22:45:11 +0300

    first task done

---
+++ JS/functions.js
@@ -0,0 +1,18 @@
+/*Write a function that accepts 2 numbers and returns -1 if the first one is smaller than the second,
+ 1 if it is vice versa, and 0 if they are equal.*/
+
+function compareNumbers() {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+
+  var result;
+  if (num1 < num2) {
+    result = -1;
+  } else if (num1 > num2) {
+    result = 1;
+  } else {
+    result = 0;
+  }
+
+  document.getElementById("compare").innerHTML = "The result is: " + result;
+}
