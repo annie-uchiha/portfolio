7f65a08b5163c7f36ea50b952b692b3c9d70b095
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-04-06 22:32:43 +0300

    objects in js - homework

---
+++ JS/objects.js
@@ -0,0 +1,21 @@
+/* 1. Create an object that describes a car (manufacturer, model, year of release, average speed)
+ and the following methods for working with this object:
+
+- A method that displays the car info 
+- A method for counting a time frame necessary to cover a given distance with the average speed.
+ Note that every 4 hours the driver has to take a 1-hour break.*/
+
+ const car1 = {
+    manufacturer: "Chevrolet",
+    model: "Impala",
+    year: 1967;
+    averageSpeed: "120 km/h",
+    displayInfo: function() {
+        console.log(Manufacturer: ${this.manufacturer});
+        console.log(Model: ${this.model});
+        console.log(Year of Release: ${this.year});
+        console.log(Average Speed: ${this.averageSpeed});
+    }
+ };
+
+ car1.displayInfo();
