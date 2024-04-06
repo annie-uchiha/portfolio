97b2df5f1994d6df21768f79e4dc68a59bec9ece
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-04-06 22:52:18 +0300

    task one from objects - complete

--- JS/objects.js
+++ JS/objects.js
@@ -5,17 +5,28 @@
 - A method for counting a time frame necessary to cover a given distance with the average speed.
  Note that every 4 hours the driver has to take a 1-hour break.*/
 
- const car1 = {
-    manufacturer: "Chevrolet",
-    model: "Impala",
-    year: 1967;
-    averageSpeed: "120 km/h",
-    displayInfo: function() {
-        console.log(Manufacturer: ${this.manufacturer});
-        console.log(Model: ${this.model});
-        console.log(Year of Release: ${this.year});
-        console.log(Average Speed: ${this.averageSpeed});
-    }
- };
+const car1 = {
+  manufacturer: "Chevrolet",
+  model: "Impala",
+  year: 1967,
+  averageSpeed: 120, // km/h
+  displayInfo: function () {
+    console.log(`Manufacturer: ${this.manufacturer}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year of Release: ${this.year}`);
+    console.log(`Average Speed: ${this.averageSpeed}`);
+  },
+  calculateTravelTime: function (distance) {
+    const hoursWithoutBreaks = distance / this.averageSpeed;
 
- car1.displayInfo();
+    const numberOfBreaks = Math.floor(hoursWithoutBreaks / 4);
+    const totalBreakTime = numberOfBreaks * 1;
+
+    const totalTime = hoursWithoutBreaks + totalBreakTime;
+
+    console.log(`Total Time to cover ${distance} km: ${totalTime} hours`);
+  },
+};
+
+car1.displayInfo();
+car1.calculateTravelTime(1500);
