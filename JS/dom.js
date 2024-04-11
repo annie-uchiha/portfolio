3293eb75de0544be8cdaae7b5b8e5017709e30e2
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-04-11 23:05:57 +0300

    moved script js for dom at bottom - it did not work right with async in head - second task almost done

--- JS/dom.js
+++ JS/dom.js
@@ -1,44 +1,66 @@
-var playList = [
-  {
-    author: "BILLIE EILISH",
-    song: "BURY A FRIEND",
-  },
-  {
-    author: "IMAGINE DRAGONS",
-    song: "NATURAL",
-  },
-  {
-    author: "EMINEM",
-    song: "LOOSE YOURSELF",
-  },
-  {
-    author: "KATE BUSH",
-    song: "RUNNING UP THAT HILL",
-  },
-  {
-    author: "THE PLATTERS",
-    song: "SIXTEEN TONS",
-  },
-  {
-    author: "TERIYAKI BOYZ",
-    song: "TOKYO DRIFT",
-  },
-  {
-    author: "NEONI",
-    song: "DARKSIDE",
-  },
-  {
-    author: "NAS",
-    song: "N Y STATE OF MIND",
-  },
-];
+document.addEventListener("DOMContentLoaded", function () {
+  // Track list functionality
+  var playList = [
+    {
+      author: "BILLIE EILISH",
+      song: "BURY A FRIEND",
+    },
+    {
+      author: "IMAGINE DRAGONS",
+      song: "NATURAL",
+    },
+    {
+      author: "EMINEM",
+      song: "LOOSE YOURSELF",
+    },
+    {
+      author: "KATE BUSH",
+      song: "RUNNING UP THAT HILL",
+    },
+    {
+      author: "THE PLATTERS",
+      song: "SIXTEEN TONS",
+    },
+    {
+      author: "TERIYAKI BOYZ",
+      song: "TOKYO DRIFT",
+    },
+    {
+      author: "NEONI",
+      song: "DARKSIDE",
+    },
+    {
+      author: "NAS",
+      song: "N Y STATE OF MIND",
+    },
+  ];
 
-var trackList = document.getElementById("trackList");
+  var trackList = document.getElementById("trackList");
 
-playList.forEach(function (track, index) {
-  var listItem = document.createElement("li");
+  playList.forEach(function (track, index) {
+    var listItem = document.createElement("li");
 
-  listItem.textContent = index + 1 + ". " + track.author + " - " + track.song;
+    listItem.textContent = index + 1 + ". " + track.author + " - " + track.song;
 
-  trackList.appendChild(listItem);
+    trackList.appendChild(listItem);
+  });
+
+  // Modal box functionality
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("openBtn");
+  var closeBtn = document.querySelector(".close");
+
+  btn.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 });
