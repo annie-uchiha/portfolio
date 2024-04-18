3b48e0138102d73115919beb0aaaff275c547341
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-04-18 23:28:07 +0300

    last two task done

--- JS/jquery-homework.js
+++ JS/jquery-homework.js
@@ -1,3 +1,5 @@
+// 1. Create a rectangle and make it move to all edges of the page after a btn has been clicked
+
 $(() => {
   $("#moveBtn").on("click", function () {
     const element = $("#movingElement");
@@ -21,6 +23,51 @@ $(() => {
           );
         });
       });
+    });
+  });
+});
+
+/* 3. Create a simple to-do list. Allow users to add and remove tasks.
+Each task should have a checkbox to mark as complete. Change style of row when task is complete.*/
+
+$(() => {
+  function addTask(taskText) {
+    $("#taskList").append(
+      '<li><input type="checkbox" class="taskCheckbox">' + taskText + "</li>"
+    );
+  }
+
+  function removeCompletedTasks() {
+    $("#taskList .taskCheckbox:checked").parent().remove();
+  }
+
+  $("#taskList").on("change", ".taskCheckbox", function () {
+    $(this).parent().toggleClass("completed");
+  });
+
+  $("#addTaskBtn").click(function () {
+    var taskText = $("#taskInput").val().trim();
+    if (taskText !== "") {
+      addTask(taskText);
+      $("#taskInput").val("");
+    }
+  });
+
+  $("#removeCompletedBtn").click(function () {
+    removeCompletedTasks();
+  });
+});
+
+//List of items and on hover details on each row
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".item");
+  items.forEach((item) => {
+    const details = item.querySelector(".details");
+    item.addEventListener("mouseenter", () => {
+      details.style.display = "block";
+    });
+    item.addEventListener("mouseleave", () => {
+      details.style.display = "none";
     });
   });
 });
