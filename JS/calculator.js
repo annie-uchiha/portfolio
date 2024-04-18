906978dfd0290b183d347f6ee9efa1be99f147f4
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-04-18 23:42:29 +0300

    calculator - using jquery - need adjusting

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
   const screen = document.querySelector(".calculator-screen");
   const buttons = [
     ...document.getElementsByClassName("value"),
@@ -43,4 +43,74 @@ document.addEventListener("DOMContentLoaded", () => {
       screen.value = "Error";
     }
   }
+});*/
+
+$(() => {
+  let currentInput = "";
+  let currentResult = "";
+  let lastOperation = "";
+
+  function updateResult() {
+    $(".calculator-screen").val(currentInput);
+  }
+
+  function clearInput() {
+    currentInput = "";
+    updateResult();
+  }
+
+  function performOperation(operation) {
+    if (currentInput !== "") {
+      if (lastOperation !== "") {
+        calculateResult();
+      }
+      currentResult = parseFloat(currentInput);
+      lastOperation = operation;
+      clearInput();
+    }
+  }
+
+  function calculateResult() {
+    if (currentInput !== "") {
+      const operand = parseFloat(currentInput);
+      switch (lastOperation) {
+        case "+":
+          currentResult += operand;
+          break;
+        case "-":
+          currentResult -= operand;
+          break;
+        case "*":
+          currentResult *= operand;
+          break;
+        case "/":
+          currentResult /= operand;
+          break;
+      }
+      currentInput = currentResult.toString();
+      lastOperation = "";
+      updateResult();
+    }
+  }
+
+  $(".value, .operator, .all-clear, .decimal, .equal-sign").click(function () {
+    const value = $(this).val();
+    switch (value) {
+      case "all-clear":
+        clearInput();
+        break;
+      case "equals":
+        calculateResult();
+        break;
+      case "add":
+      case "subtract":
+      case "multiply":
+      case "divide":
+        performOperation(value);
+        break;
+      default:
+        currentInput += value;
+        updateResult();
+    }
+  });
 });
